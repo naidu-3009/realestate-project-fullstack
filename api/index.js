237709b51fb85881f19c6d5bf68b3cb36a015489
Talari Nishanth Naidu <talarinishanth3009@gmail.com>
2024-08-21 17:39:55 +0530

    created userupdate api

--- api/index.js
+++ api/index.js
@@ -1,11 +1,13 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
+import cookieParser from 'cookie-parser';
 import userRouter from './routes/user.route.js';
 import authRouter from './routes/auth.route.js';
 dotenv.config();
 
 const app = express();
+app.use(cookieParser());
 
 // Middleware to parse JSON
 app.use(express.json());
@@ -22,6 +24,9 @@ mongoose.connect(process.env.MONGO)
     app.use('/api/user', userRouter);
     app.use('/api/auth', authRouter);
 // Start the server
+
+
+
 app.listen(3000, () => {
     console.log('Server is running on port 3000!');
   });
