23f5bc6b977746c6aa3f532998e5163ddf603f57
Talari Nishanth Naidu <talarinishanth3009@gmail.com>
2024-08-24 01:15:48 +0530

    completed listing page functionality excluding mesage part

--- client/src/pages/UpdateListing.jsx
+++ client/src/pages/UpdateListing.jsx
@@ -295,7 +295,10 @@ export default function CreateListing() {
               />
               <div className="flex flex-col items-center">
                 <p>Regular-Price</p>
-                <span className="text-s">(₹ / month)</span>
+                {/* <span className="text-s">(₹ / month)</span> */}
+                {formData.type === "rent" && (
+                  <span className="text-xs">(₹ / month)</span>
+                )}
               </div>
             </div>
             {formData.offer && (
