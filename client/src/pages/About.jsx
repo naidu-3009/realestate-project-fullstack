517c446f9f5a396fe562ea6e76f96399f563020a
Talari Nishanth Naidu <talarinishanth3009@gmail.com>
2024-08-25 01:16:57 +0530

    added about page

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,9 +1,40 @@
-import React from 'react'
+import React from "react";
 
 export default function about() {
   return (
-    <h1>
-      About
-    </h1>
-  )
+    <div className=" py-10 px-4 max-w-6xl mx-auto">
+      <h1 className="text-3xl font-bold mb-4 text-blue-800">
+        {" "}
+        About VistaraHomes
+      </h1>
+      <p className="mb-4 text-slate-700 font-semibold">
+        Welcome to VistaraHomes, your trusted platform for all real estate
+        needs. Whether you're looking to rent, buy, or sell properties,
+        VistaraHomes offers a seamless and user-friendly experience tailored
+        just for you. Our platform is designed to make finding your dream home
+        or showcasing your property as easy and efficient as possible. With a
+        wide array of listings across diverse locations, we ensure that you have
+        access to the best opportunities in the real estate market.
+      </p>
+      <p className="mb-4 text-slate-700 font-semibold">
+        At VistaraHomes, we prioritize convenience and transparency. Our
+        platform allows you to directly connect with landlords and property
+        owners, ensuring clear communication and a hassle-free process. Whether
+        you’re a homeowner wanting to list your property or a tenant searching
+        for the perfect place to call home, our robust set of tools and
+        resources are here to guide you every step of the way. We are committed
+        to providing a secure environment where you can make informed decisions
+        with confidence.
+      </p>
+      <p className="mb-4 text-slate-700 font-semibold">
+        Our mission is to revolutionize the way people interact with real
+        estate. We believe in empowering our users with the tools and
+        information they need to make smart real estate decisions. VistaraHomes
+        is not just a marketplace; its a community where buyers, sellers,
+        landlords, and tenants come together to achieve their goals. Join us
+        today and experience the future of real estate, where your satisfaction
+        is our top priority.
+      </p>
+    </div>
+  );
 }
