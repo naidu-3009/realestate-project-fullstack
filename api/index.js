170d36e449e85135f83f65dea2b70f88ace81009
Talari Nishanth Naidu <talarinishanth3009@gmail.com>
2024-08-24 19:40:55 +0530

    added search button functionality in search page

--- api/index.js
+++ api/index.js
@@ -1,10 +1,10 @@
-import express from 'express';
-import mongoose from 'mongoose';
-import dotenv from 'dotenv';
-import cookieParser from 'cookie-parser';
-import userRouter from './routes/user.route.js';
-import authRouter from './routes/auth.route.js';
-import listingRouter from './routes/listing.route.js'
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+import userRouter from "./routes/user.route.js";
+import authRouter from "./routes/auth.route.js";
+import listingRouter from "./routes/listing.route.js";
 dotenv.config();
 
 const app = express();
@@ -14,30 +14,29 @@ app.use(cookieParser());
 app.use(express.json());
 
 // Database connection
-mongoose.connect(process.env.MONGO)
-    .then(() => {
-        console.log("Connected to database");
-    })
-    .catch((err) => {
-        console.log(err);
-    });
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log("Connected to database");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
-    app.use('/api/user', userRouter);
-    app.use('/api/auth', authRouter);
-    app.use('/api/listing',listingRouter)
+app.use("/api/user", userRouter);
+app.use("/api/auth", authRouter);
+app.use("/api/listing", listingRouter);
 // Start the server
 
-
-
 app.listen(3000, () => {
-    console.log('Server is running on port 3000!');
-  });
+  console.log("Server is running on port 3000!");
+});
 app.use((err, req, res, next) => {
-    const statusCode = err.statusCode || 500;
-    const message = err.message || 'Internal Server Error';
-    return res.status(statusCode).json({
-      success: false,
-      statusCode,
-      message,
-    });
+  const statusCode = err.statusCode || 500;
+  const message = err.message || "Internal Server Error";
+  return res.status(statusCode).json({
+    success: false,
+    statusCode,
+    message,
   });
+});
