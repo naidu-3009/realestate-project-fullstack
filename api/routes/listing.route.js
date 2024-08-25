6806bba8ca14b869c9f99013df56c112e0c32d1c
Talari Nishanth Naidu <talarinishanth3009@gmail.com>
2024-08-25 15:39:40 +0530

    some minor changes

--- api/routes/listing.route.js
+++ api/routes/listing.route.js
@@ -1,7 +1,6 @@
 import express from "express";
 import { createListing,deleteListing,updateListing,getListing,getListings } from "../controllers/listing.controller.js";
 import { verifyToken } from "../utils/verifyUser.js";
-import { RouterProvider } from "react-router-dom";
 const router = express.Router();
 
 router.post("/create",verifyToken, createListing);
